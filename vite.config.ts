b4fe4f2ed61eefda744be7e8911665c3b63c548a
Vladislav Akulinin <vwinterdev@gmail.com>
2026-01-17 11:47:49 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  
+  plugins: [vue(), tailwindcss(), VitePWA({
+    injectRegister: 'auto',
+    registerType: 'autoUpdate',
+  })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
